86adcb488a7a18fc3fe5e58401fd6c65d102da3f
Isha <isha.nesa@gmail.com>
2024-07-26 11:56:33 -0400

    made a teensy tiny change to the color

--- AllAboutMe/ContentView.swift
+++ AllAboutMe/ContentView.swift
@@ -22,12 +22,14 @@ struct ContentView: View {
                         .scenePadding(.top)
                         .foregroundStyle(Color(red:0.219, green: 0.592, blue: 0.6))
                     Text("Current Kode With Klossy Scholar")                    .foregroundStyle(Color(red:0.219, green: 0.592, blue: 0.6))
-                    Text("I want to be a Computer Scientist, I don't know what I want to go into specifically and am exploring my options (especially with this camp). I also love to travel, I have been to at least 15 countries. I am also very adventurous and love to take risks (my friends consider me a dare-devil)")
+                Text(":)")
+                    Text("I want to be a Computer Scientist, I don't know what I want to go into specifically and am exploring my options (especially with this camp). I also love to travel, I have been to at least 15 countries. I am also very adventurous and love to take risks (my friends consider me a dare-devil).")
+                    
                         .padding()
                         .foregroundStyle(Color(red:0.25, green: 0.22, blue: 0.6))
                 }
                 .background(Rectangle()
-                    .foregroundColor(Color(red: 0.945, green: 0.678, blue: 1)))
+                    .foregroundColor(Color(red: 0.945, green: 0.678, blue: 0.9)))
                     .cornerRadius(20)
                     .scenePadding(.horizontal)
 
