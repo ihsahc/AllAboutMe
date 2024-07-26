01439dfe558484460d29a43349628d08c2510415
Isha <isha.nesa@gmail.com>
2024-07-26 13:05:11 -0400

    deleted the smiley face

--- AllAboutMe/ContentView.swift
+++ AllAboutMe/ContentView.swift
@@ -22,7 +22,6 @@ struct ContentView: View {
                         .scenePadding(.top)
                         .foregroundStyle(Color(red:0.219, green: 0.592, blue: 0.6))
                     Text("Current Kode With Klossy Scholar")                    .foregroundStyle(Color(red:0.219, green: 0.592, blue: 0.6))
-                Text(":)")
                     Text("I want to be a Computer Scientist, I don't know what I want to go into specifically and am exploring my options (especially with this camp). I also love to travel, I have been to at least 15 countries. I am also very adventurous and love to take risks (my friends consider me a dare-devil).")
                     
                         .padding()
